cddaa7e9fcef0bb8e7878dca03066ff4191628a0
ThatGuy10000 <64559536+ThatGuy10000@users.noreply.github.com>
2020-04-29 18:16:21 -0400

    Update Project_Code_No_IC.ino
    Added LED display variable and code. LED will be off if SpO2 is greater than 90%, flash if between 80-90%, and stay on if under 80%

--- Project_Code_No_IC.ino
+++ Project_Code_No_IC.ino
@@ -11,6 +11,7 @@ const int SDAPin = A4;
 const int SCLPin = A5;
 const int LED_RED = 2;
 const int LED_IR = 3;
+const int LED_Display = 4;
 const int detectorPin = A0;
 
 //Declare Variables for Calculation
@@ -26,8 +27,9 @@ void setup() {
   Serial.begin(9600); //Start Serial Communication with 9600 Baud Rate
   analogReference(INTERNAL); //Sets Reference to 1.1V
   
-  pinMode(LED_RED, OUTPUT); //Sets LED_RED as an output pin
-  pinMode(LED_IR, OUTPUT); //Sets LED_IR as an output pin
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_IR, OUTPUT);
+  pinMode(LED_Display, OUTPUT); //Sets LED_RED, LED_IR, and LED_Display as an output pin
   pinMode(detectorPin, INPUT); //Sets detectorPin as an input pin
 }
 
@@ -35,19 +37,25 @@ void loop() {
   float redVal = readAbsorbance("Red"); //Store absorbance voltage for red LED in "redVal"
   float irVal = readAbsorbance("IR"); //Store absorbance voltage for IR LED in "irVal"
 
+  if(SpO2 > 80)
+    digitalWrite(LED_Display, LOW); //Turns off LED if SpO2 is greater than 80%
+  
   if(redVal > redAC)
     redAC = redVal; //Makes redAC the max value for the red LED
 
   if(redVal < redDC)
     redDC = redVal; //Makes redDC the min value for the red LED
 
+  if(SpO2 < 90)
+    digitalWrite(LED_Display, HIGH); //Turns on LED if SpO2 is less than 90%. LED will flash if between 80-90%, stay off if over 90%, and stay on if under 80%
+  
   if(irVal > irAC)
     irAC = irVal; //Makes irAC the max value for the IR LED
 
   if(irVal < irDC)
     irDC = irVal; //Makes irDC the min value for the IR LED
 
-  count++;
+  count++; //Increments the count variable
 
   if(count >= collectedSamples)
   {
